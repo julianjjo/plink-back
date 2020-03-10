9d6d75174276a1efde20ea0e89ffceedb955ea99
Julian Mican <julian.mican@sophossolutions.com>
2020-03-10 16:26:24 -0500

    coverage real

--- server/src/__tests__/utils/authtest.js
+++ server/src/__tests__/utils/authtest.js
@@ -1,42 +1,43 @@
 import Auth from '../../utils/auth';
+describe("Authentication", function() {
+    describe("verify token", function() {
+        let token, auth;
 
-describe("Authentication verify token", function() {
-    let token, auth;
+        beforeEach(() => {
+            this.auth =  new Auth();
+        });
 
-    beforeEach(() => {
-        this.auth =  new Auth();
-    });
+        test('is false', () => {
+            expect(this.auth.verify('test')).toBeFalsy();
+        });
 
-    test('is false', () => {
-        expect(this.auth.verify('test')).toBeFalsy();
-    });
+        beforeEach(() => {
+            token = this.auth.sign({ user: 'julian', password: 'ndjnejnejnje' });
+        });
 
-    beforeEach(() => {
-        token = this.auth.sign({ user: 'julian', password: 'ndjnejnejnje' });
-    });
+        test('is true', () => {
+            expect(this.auth.verify(token)).toBeTruthy();
+        });
+    })
 
-    test('is true', () => {
-        expect(this.auth.verify(token)).toBeTruthy();
-    });
+    describe("get token", function() {
+        let auth;
+
+
+        beforeEach(() => {
+             this.auth =  new Auth();
+        });
+
+        test('is 3df5dc', () => {
+            const mockRequest = () => {
+                const req = {};
+                req.headers = {};
+                req.headers.authorization = "token 3df5dc";
+                return req;
+            };
+            let req = mockRequest();
+            expect(this.auth.getToken(req)).toBe("3df5dc");
+        });
+    })
+
 })
-
-describe("Authentication get token", function() {
-    let auth;
-
-
-    beforeEach(() => {
-         this.auth =  new Auth();
-    });
-
-    test('is 3df5dc', () => {
-        const mockRequest = () => {
-            const req = {};
-            req.headers = {};
-            req.headers.authorization = "token 3df5dc";
-            return req;
-        };
-        let req = mockRequest();
-        expect(this.auth.getToken(req)).toBe("3df5dc");
-    });
-})
-
